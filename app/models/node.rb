cd1264304a0a89ab203d33fe2ef7fe13e47f3e67
mac tichner <mst209@gmail.com>
2024-10-21 18:46:30 -0400

    node

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,24 +1,23 @@
 # frozen_string_literal: true
 
 class Node < ApplicationRecord
-
   has_many :birds
 
   def self_and_ancestors
-    # to_do: parameratize inputs 
-    Node.joins("join get_ancestors_and_self(#{self.id}) ancestors on nodes.id = ancestors.id").order("ancestors.depth")
+    # to_do: parameratize inputs
+    Node.joins("join get_ancestors_and_self(#{id}) ancestors on nodes.id = ancestors.id").order('ancestors.depth')
   end
 
   def ancestors
-    Node.joins("join get_ancestors(#{self.id}) ancestors on nodes.id = ancestors.id").order("ancestors.depth")
+    Node.joins("join get_ancestors(#{id}) ancestors on nodes.id = ancestors.id").order('ancestors.depth')
   end
 
   def self_and_descendants
-    Node.joins("join get_descendants_and_self(#{self.id}) ancestors on nodes.id = ancestors.id").order("ancestors.depth")
+    Node.joins("join get_descendants_and_self(#{id}) ancestors on nodes.id = ancestors.id").order('ancestors.depth')
   end
 
   def descendants
-    Node.joins("join get_descendants(#{self.id}) descendants on nodes.id = descendants.id").order("descendants.depth")
+    Node.joins("join get_descendants(#{id}) descendants on nodes.id = descendants.id").order('descendants.depth')
   end
 
   def root
@@ -30,11 +29,12 @@ class Node < ApplicationRecord
   end
 
   def common_ancestors(another_node)
-    self_and_ancestors.joins("join get_ancestors_and_self(#{another_node.id}) ancestors2 on ancestors.id = ancestors2.id").order("ancestors.depth")
+    self_and_ancestors.joins("join get_ancestors_and_self(#{another_node.id}) ancestors2 on ancestors.id = ancestors2.id").order('ancestors.depth')
   end
 
   def lowest_common_ancestor(another_node)
     return nil if another_node.nil?
+
     common_ancestors(another_node).first
   end
 
@@ -44,9 +44,12 @@ class Node < ApplicationRecord
 
     if !a.nil? && !b.nil?
       lowest_common_ancestor = a.lowest_common_ancestor(b)
-      return { root_id: lowest_common_ancestor.root.id, lowest_common_ancestor: lowest_common_ancestor.id, depth: lowest_common_ancestor.depth } unless lowest_common_ancestor.nil?
+      unless lowest_common_ancestor.nil?
+        return { root_id: lowest_common_ancestor.root.id, lowest_common_ancestor: lowest_common_ancestor.id,
+                 depth: lowest_common_ancestor.depth }
+      end
     end
-    { root_id: nil, lowest_common_ancestor: nil, depth: nil}
+    { root_id: nil, lowest_common_ancestor: nil, depth: nil }
   end
 
   def self.descendant_ids(node_ids)
